68eba55e3fe20655a016ec94475b704c58aedf66
RVlad <reshetov664@gmail.com>
2025-06-08 17:48:54 +0700

    hw_24 - 2

--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,3 @@
-# from django.shortcuts import render, get_object_or_404
-# from .models import Product
-#
-# def home(request):
-#     products = Product.objects.all()
-#     return render(request, 'home.html', {'products': products})
-#
-# def contacts(request):
-#     return render(request, 'contacts.html')
-#
-# def product_detail(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     return render(request, 'product_detail.html', {'product': product})
-
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
