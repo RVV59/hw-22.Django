22d949e74b89407456cfa11c0974e9a3d9c88327
RVlad <reshetov664@gmail.com>
2025-06-08 17:45:42 +0700

    hw_24 - 1

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
+# from django.shortcuts import render, get_object_or_404
+# from .models import Product
+#
+# def home(request):
+#     products = Product.objects.all()
+#     return render(request, 'home.html', {'products': products})
+#
+# def contacts(request):
+#     return render(request, 'contacts.html')
+#
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     return render(request, 'product_detail.html', {'product': product})
+
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
@@ -5,9 +19,9 @@ def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
 
-def contacts(request):
-    return render(request, 'contacts.html')
-
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'product_detail.html', {'product': product})
+
+def contacts(request):
+    return render(request, 'contacts.html')
--- hw_22/settings.py
+++ hw_22/settings.py
@@ -107,9 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Медиатека (Media)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
@@ -135,3 +133,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Медиатека (Media)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
