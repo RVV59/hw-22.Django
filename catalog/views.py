8767b2b9f77811f6455b3c8493a574adc612a311
RVV59 <reshetov664@gmail.com>
2025-06-16 20:45:51 +0700

    проект перенесен на планшет, наконец-то

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView, DetailView, TemplateView
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+
+
 
 class HomeView(ListView):
     model = Product
