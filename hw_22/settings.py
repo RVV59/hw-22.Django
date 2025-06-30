de1f5f68170a2e94729084f88f072f5302e4d7ba
RVV59 <reshetov664@gmail.com>
2025-06-29 18:01:46 -0700

    hw_27 finished unday, une 29, 2025 6:01:15

--- hw_22/settings.py
+++ hw_22/settings.py
@@ -33,10 +33,11 @@ DEBUG = True
 
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
-
+AUTH_USER_MODEL = 'users.CustomUser'
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'blog',
     'catalog',
     'django.contrib.admin',
---
+++ users/models.py
@@ -0,0 +1,46 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, phone=None, country=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        user = self.model(
+            email=email,
+            phone=phone,
+            country=country,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, phone=None, country=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, phone, country, **extra_fields)
+
+class CustomUser(AbstractUser):
+    username = None  # Убираем username
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True, null=False)
+    country = models.CharField(max_length=100, blank=True, null=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
